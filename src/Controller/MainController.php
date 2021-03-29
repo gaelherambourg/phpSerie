bc98560c5c0cf7e18cfb37bfe25cbf27143aba77
gherambourg <g.heambourg.gh@gmail.com>
2021-03-29 11:28:43 +0200

    Premiere page

---
+++ src/Controller/MainController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class MainController extends AbstractController
+{
+   
+    /**
+     * @Route("/", name="home")
+     */
+    public function home(){
+
+        return $this->render('base.html.twig');
+
+    }
+
+    
+
+}
